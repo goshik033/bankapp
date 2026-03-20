56d8766a64afe64a3cb43534a5da63c23a2a7989
Kolidgio <sk.gosha@yandex.ru>
2026-03-20 16:09:09 +0700

    feat: добавить базовый модуль front-ui с thymeleaf страницами

---
+++ front-ui/src/main/java/ru/kolidgio/bankapp/frontui/FrontUiApplication.java
@@ -0,0 +1,12 @@
+package ru.kolidgio.bankapp.frontui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontUiApplication.class, args);
+    }
+}
---
+++ front-ui/src/main/java/ru/kolidgio/bankapp/frontui/controller/PageController.java
@@ -0,0 +1,23 @@
+package ru.kolidgio.bankapp.frontui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";
+    }
+
+    @GetMapping({"/", "/home"})
+    public String homePage() {
+        return "home";
+    }
+}
